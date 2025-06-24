1f4dc9d1112eb52951d484c38fafa71706c05f54
igaserx <gasseralaa11@gmail.com>
2025-06-24 20:41:34 +0300

    hive init

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,5 +1,11 @@
-class Note {
-  final  String title;
-  final String content ;
+import 'package:hive/hive.dart';
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class Note extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String content;
   Note({required this.title, required this.content});
 }
