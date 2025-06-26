2dc41568be2190be0afd5ec2e23c89195be02bee
igaserx <gasseralaa11@gmail.com>
2025-06-26 19:38:11 +0300

    provid the cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:note_app/core/constant.dart';
+import 'package:note_app/cubits/cubit/add_note_cubit.dart';
 import 'package:note_app/models/note_model.dart';
 
 import 'package:note_app/views/home_view.dart';
 
 void main() async {
   await Hive.initFlutter();
-  await Hive.openBox("note_box");
+  await Hive.openBox(kBox);
   Hive.registerAdapter(NoteAdapter());
   runApp(const MyApp());
 }
@@ -16,10 +19,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(brightness: Brightness.light),
-      home: HomeView(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => AddNoteCubit(),)
+      ],
+      child: MaterialApp(
+        
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(brightness: Brightness.light),
+        
+        home: HomeView(),
+      ),
     );
   }
 }
